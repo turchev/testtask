d642d21e32679eb877c0cfd876137b8711359a1d
aspis <turchev@gmail.com>
2018-08-16 16:48:48 +0400

    commit Чт авг 16 16:48:48 +04 2018

--- src/main/java/com/haulmont/testtask/data/DBConnection.java
+++ src/main/java/com/haulmont/testtask/data/DBConnection.java
@@ -14,7 +14,7 @@ public class DBConnection {
 			Class.forName("org.hsqldb.jdbc.JDBCDriver");
 			System.out.println("HSQLDB JDBCDriver Loaded");
 			con = DriverManager.getConnection(
-					"jdbc:hsqldb:file:car_service_db;shutdown=true;ifexists=false", "SA", "");
+					"jdbc:hsqldb:file:db/car_service_db;shutdown=true;ifexists=false", "SA", "");
 			System.out.println("HSQLDB Connection Created");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
