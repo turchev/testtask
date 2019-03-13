13013bc4ad0a0af0944ad7b975d64651e3ff9302
aspis <turchev@gmail.com>
2019-03-13 16:26:12 +0400

    commit Ср мар 13 16:26:12 +04 2019

--- src/main/java/com/haulmont/testtask/view/MechanicWindowStat.java
+++ src/main/java/com/haulmont/testtask/view/MechanicWindowStat.java
@@ -1,21 +1,21 @@
 package com.haulmont.testtask.view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.dussan.vaadin.dcharts.DCharts;
-import org.dussan.vaadin.dcharts.base.elements.XYaxis;
-import org.dussan.vaadin.dcharts.data.DataSeries;
-import org.dussan.vaadin.dcharts.data.Ticks;
-import org.dussan.vaadin.dcharts.metadata.renderers.AxisRenderers;
-import org.dussan.vaadin.dcharts.metadata.renderers.SeriesRenderers;
-import org.dussan.vaadin.dcharts.options.Axes;
-import org.dussan.vaadin.dcharts.options.Highlighter;
-import org.dussan.vaadin.dcharts.options.Options;
-import org.dussan.vaadin.dcharts.options.SeriesDefaults;
 
+import com.byteowls.vaadin.chartjs.ChartJs;
+import com.byteowls.vaadin.chartjs.config.BarChartConfig;
+import com.byteowls.vaadin.chartjs.data.BarDataset;
+import com.byteowls.vaadin.chartjs.data.Dataset;
+import com.byteowls.vaadin.chartjs.options.Position;
+import com.byteowls.vaadin.chartjs.options.elements.Rectangle.RectangleEdge;
 import com.haulmont.testtask.dao.DaoFactory;
 import com.haulmont.testtask.dao.MechanicDao;
 import com.haulmont.testtask.ds.DsType;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.Window;
 
 @SuppressWarnings("serial")
@@ -30,51 +30,39 @@ public class MechanicWindowStat extends Window {
 			this.id = id;
 			hsqlDaoFactory = DaoFactory.getFactory(DsType.HSQLDB);
 			mechanicDao = hsqlDaoFactory.getMechanicDao();
-
-//			DataSeries dataSeries = new DataSeries().add(322);
-//			SeriesDefaults seriesDefault = new SeriesDefaults().setRenderer(SeriesRenderers.METER_GAUGE)
-//					.setRendererOptions(
-//							new MeterGaugeRenderer().setMin(100).setMax(500).setIntervals(200, 300, 400, 500)
-//									.setIntervalColors("#66cc66", "#93b75f", "#E7E658", "#cc6666"));
-//
-//			Options options = new Options().setSeriesDefaults(seriesDefault);
-//
-////			DCharts chart = new DCharts().setDataSeries(dataSeries).setOptions(options).show();
-//			DCharts chart = new DCharts().setDataSeries(dataSeries).setOptions(options);
-////			HorizontalLayout hLayout = new HorizontalLayout(btnApple, btnCancel);
-////			vlLayout.addComponent(hLayout);
-//			this.setContent(chart);
-
-			DataSeries dataSeries = new DataSeries()
-					.add(2, 6, 7, 10);
-
-				SeriesDefaults seriesDefaults = new SeriesDefaults()
-					.setRenderer(SeriesRenderers.BAR);
-
-				Axes axes = new Axes()
-					.addAxis(
-						new XYaxis()
-							.setRenderer(AxisRenderers.CATEGORY)
-							.setTicks(
-								new Ticks()
-									.add("a", "b", "c", "d")));
-
-				Highlighter highlighter = new Highlighter()
-					.setShow(false);
-
-				Options options = new Options()
-					.setSeriesDefaults(seriesDefaults)
-					.setAxes(axes)
-					.setHighlighter(highlighter);
-
-				DCharts chart = new DCharts()
-					.setDataSeries(dataSeries)
-					.setOptions(options)
-					.show();
-				
+//			VerticalLayout vlLayout = new VerticalLayout(getChart());
+//			this.setContent(vlLayout);
 		} catch (Exception e) {
 			throw new UiException(e);
 		}
 		LOG.debug("Created MechanicWindowStat");
 	}
+
+	public Component getChart() {
+		BarChartConfig barConfig = new BarChartConfig();
+		barConfig.horizontal();
+		barConfig.data().labels("January", "February", "March", "April", "May", "June", "July")
+				.addDataset(new BarDataset().backgroundColor("rgba(220,220,220,0.5)").label("Dataset 1"))
+				.addDataset(new BarDataset().backgroundColor("rgba(151,187,205,0.5)").label("Dataset 2").hidden(true))
+				.addDataset(new BarDataset().backgroundColor("rgba(151,187,205,0.5)").label("Dataset 3")).and()
+				.options().responsive(true).title().display(true).text("Chart.js Horizontal Bar Chart").and().elements()
+				.rectangle().borderWidth(2).borderColor("rgb(0, 255, 0)").borderSkipped(RectangleEdge.LEFT).and().and()
+				.legend().fullWidth(false).position(Position.LEFT).and().done();
+
+		List<String> labels = barConfig.data().getLabels();
+		for (Dataset<?, ?> ds : barConfig.data().getDatasets()) {
+			BarDataset lds = (BarDataset) ds;
+			List<Double> data = new ArrayList<>();
+			for (int i = 0; i < labels.size(); i++) {
+				data.add((double) (Math.random() > 0.5 ? 1.0 : -1.0) * Math.round(Math.random() * 100));
+			}
+			lds.dataAsList(data);
+		}
+
+		ChartJs chart = new ChartJs(barConfig);
+		chart.setJsLoggingEnabled(true);
+//        chart.addClickListener((a,b) -> DemoUtils.notification(a, b, barConfig.data().getDatasets().get(a)));
+		return chart;
+	}
+
 }
