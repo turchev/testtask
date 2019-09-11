9384b658418a97edddc99ba7be3fedaf45c6a0fd
aspis <turchev@gmail.com>
2019-09-11 16:29:13 +0400

    Commit Ср сен 11 16:29:13 +04 2019

--- src/main/java/com/github/turchev/carrepairshop/view/ErrorView.java
+++ src/main/java/com/github/turchev/carrepairshop/view/ErrorView.java
@@ -1,26 +1,24 @@
 package com.github.turchev.carrepairshop.view;
 
 import com.github.turchev.carrepairshop.MainLayout;
-import com.vaadin.annotations.Theme;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.navigator.View;
-import com.vaadin.ui.Alignment;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.flow.theme.Theme;
 
-@Theme("mytheme")
 @SuppressWarnings("serial")
 @Route(value = ErrorView.NAME, layout = MainLayout.class)
 @PageTitle(ErrorView.NAME)
-public class ErrorView extends VerticalLayout implements View {
+public class ErrorView extends VerticalLayout {
 	public static final String NAME = "error";
-
-	public ErrorView() {
-		this.setSizeFull();	
-		this.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
-		Label lblEroor = new Label("ERROR");		
-		lblEroor.addStyleName("errorLabel");	
-		this.addComponent(lblEroor);
-	}
+//
+//	public ErrorView() {
+//		this.setSizeFull();	
+//		this.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+//		Label lblEroor = new Label("ERROR");		
+//		lblEroor.addStyleName("errorLabel");	
+//		this.addComponent(lblEroor);
+//	}
 }
